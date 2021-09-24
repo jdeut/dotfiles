a3e87e8f16d8d792822f81d01a210210f7e697f2
Johannes Deutsch <johannes.deutsch@uni-due.de>
2021-09-24 17:13:13 +0200

    don't shade toggleterm-inals

--- neovim/.config/nvim/lua/pluginconfig/toggleterm.lua
+++ neovim/.config/nvim/lua/pluginconfig/toggleterm.lua
@@ -1,7 +1,8 @@
 require'toggleterm'.setup{
-    close_on_exit = false,-- close the terminal window when
+    close_on_exit = false, -- close the terminal window when
     start_in_insert = false,
-    open_mapping = [[<F8>]]
+    open_mapping = [[<F8>]],
+    shade_terminals = false
 }
 
 toggleterminals = {}
