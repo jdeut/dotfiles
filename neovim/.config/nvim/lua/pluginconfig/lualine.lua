50819b71bff99707e0d2ac8988580f20ce1ebc77
Johannes Deutsch <johannes.deutsch@uni-due.de>
2021-09-24 14:09:24 +0200

    change statusline style if winwidth to small

--- neovim/.config/nvim/lua/pluginconfig/lualine.lua
+++ neovim/.config/nvim/lua/pluginconfig/lualine.lua
@@ -1,26 +1,43 @@
+local function hide_on_winwidth_cond()
+   if vim.fn.winwidth(0) < 80 then
+      return false
+   else 
+      return true 
+   end 
+end
+
+local sections_common = {
+    lualine_c = { [[vim.fn.expand("%")]] },
+    lualine_x = {},
+    lualine_y = {{
+       [[encoding]] , [[filetype]], [["w" .. vim.fn.winnr() .. "b" .. vim.fn.bufnr()]], [[progress]],
+       cond = hide_on_winwidth_cond
+    }},
+    lualine_z = {{
+       [[location]],
+       cond = hide_on_winwidth_cond
+    }}
+}
+
+local sections = vim.tbl_deep_extend('error', {
+      lualine_a = {{[[mode]], fmt = function(s) return s:sub(1,1) end }},
+      lualine_b = {{[[branch]], cond = hide_on_winwidth_cond }}
+   }, sections_common
+)
+
+local inactive_sections = vim.tbl_deep_extend('keep', {
+      lualine_a = {},
+      lualine_b = {},
+   }, sections_common
+)
+
 
 require('lualine').setup({
     options = {
        component_separators = { left = [[]] , right = [[]] },
-        section_separators = { left = [[]] , right = [[]] },
-        icons_enabled = true
+          section_separators = { left = [[]] , right = [[]] },
+          icons_enabled = true
     },
-    sections = {
-        lualine_a = { [[mode]] },
-        lualine_b = { [[branch]] },
-        lualine_c = { [[vim.fn.expand("%")]] },
-        lualine_x = { [[encoding]] , [[filetype]] },
-        lualine_y = { [["Win" .. vim.fn.winnr()]], [["Buf" .. vim.fn.bufnr()]], [[progress]] },
-        lualine_z = { [[location]] }
-    },
-    inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { [[vim.fn.expand("%")]] },
-        lualine_x = { [[location]] },
-        lualine_y = { [["Win" .. vim.fn.winnr()]], [["Buf" .. vim.fn.bufnr()]], [[progress]] },
-        lualine_z = {}
-    }
+    sections = sections,
+    inactive_sections = inactive_sections
 })
-    --}
---}
