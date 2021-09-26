98c3572470b38bdaca02765a7a80f3c89d3be2e0
Johannes Deutsch <johannes.deutsch@uni-due.de>
2021-09-26 13:11:58 +0200

    add/edit fzf widgets

--- zsh/.zsh/fzf/fzf.zsh
+++ zsh/.zsh/fzf/fzf.zsh
@@ -4,21 +4,36 @@ __fzfcmd() {
 }
 
 fzf-history-widget() {
-  local selected num
-  setopt localoptions noglobsubst noposixbuiltins pipefail no_aliases 2> /dev/null
-  selected=( $(fc -rl 1 | perl -ne 'print if !$seen{(/^\s*[0-9]+\**\s+(.*)/, $1)}++' |
-    FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --bind=ctrl-r:toggle-sort $FZF_CTRL_R_OPTS --query=${(qqq)LBUFFER} +m" $(__fzfcmd)) )
-  local ret=$?
-  if [ -n "$selected" ]; then
-    num=$selected[1]
-    if [ -n "$num" ]; then
-      zle vi-fetch-history -n $num
+    local selected num
+    setopt localoptions noglobsubst noposixbuiltins pipefail no_aliases 2> /dev/null
+    # perl -ne 'print if !$seen{(/^\s*[0-9]+\**\s+(.*)/, $1)}++' | \
+    selected=( $(
+            fc -rln 1 | \
+            FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --bind=ctrl-r:toggle-sort,ctrl-z:ignore $FZF_CTRL_R_OPTS --query=${(qqq)LBUFFER} +m" \
+            $(__fzfcmd)
+        )
+    )
+    local ret=$?
+    if [ -n "$selected" ]; then
+        num=$selected[1]
+        if [ -n "$num" ]; then
+            zle vi-fetch-history -n $num
+        fi
     fi
-  fi
-  zle reset-prompt
-  return $ret
+    zle reset-prompt
+    return $ret
 }
-zle     -N    fzf-history-widget
+zle     -N   fzf-history-widget
+
+function fzf-cdr() {
+    local selected_dir=$(cdr -l | perl -lane 'print join q{ }, @F[1..$#F]' | fzf --ansi --query "$LBUFFER")
+    if [ -n "$selected_dir" ]; then
+        BUFFER="cd ${selected_dir}"
+        zle accept-line
+    fi
+    zle clear-screen
+}
+zle -N fzf-cdr
 
 fzf-cd-widget() {
   local cmd="${FZF_ALT_C_COMMAND:-"command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
