3e19d203d1cfb08166959c2225bcf2d4556b5456
Johannes Deutsch <johannes.deutsch@uni-due.de>
2021-09-26 15:42:56 +0200

    fix

--- neovim/.config/nvim/lua/au-hook-colorscheme.lua
+++ neovim/.config/nvim/lua/au-hook-colorscheme.lua
@@ -1,91 +1,130 @@
 local t = {}
 
+t.get_hl_group_colors = function (hl_gr) 
+    local tbl_color = {}
+
+    local hl_id = vim.fn.hlID(hl_gr)
+    local hl_id_trans = vim.fn.synIDtrans(hl_id)
+
+    tbl_color.fg = vim.fn.synIDattr(hl_id_trans, 'fg')
+    tbl_color.bg = vim.fn.synIDattr(hl_id_trans, 'bg')
+
+    return tbl_color
+end
+
+
+vim.g.terminal_color_0  = '#2e3436'
+vim.g.terminal_color_1  = '#cc0000'
+vim.g.terminal_color_2  = '#4e9a06'
+vim.g.terminal_color_3  = '#c4a000'
+vim.g.terminal_color_4  = '#3465a4'
+vim.g.terminal_color_5  = '#75507b'
+vim.g.terminal_color_6  = '#0b939b'
+vim.g.terminal_color_7  = '#d3d7cf'
+vim.g.terminal_color_8  = '#555753'
+vim.g.terminal_color_9  = '#ef2929'
+vim.g.terminal_color_10 = '#8ae234'
+vim.g.terminal_color_11 = '#fce94f'
+vim.g.terminal_color_12 = '#729fcf'
+vim.g.terminal_color_13 = '#ad7fa8'
+vim.g.terminal_color_14 = '#00f5e9'
+vim.g.terminal_color_15 = '#eeeeec'
+
 local syntax = {
-   Folded           = { fg = '#000000', bg = '#bfbfbf', style = 'italic' },
-   FoldColumn       = { fg = 'red', bg = 'blue', style = 'bold' },
-   VertSplit        = { bg = '#a7a7a7', fg = '#b000a0', style = 'bold' },
+    Folded           = { fg = '#000000', bg = '#bfbfbf', style = 'italic' },
+    FoldColumn       = { fg = 'red', bg = 'blue', style = 'bold' },
+    VertSplit        = { bg = '#a7a7a7', fg = '#b000a0', style = 'bold' },
 
-   WildMenu         = { bg = 'none', fg = '#00ff00', style = 'italic' },
+    IndentBlankline  = { fg = '#f740a4' },
 
-   RegTitle         = { fg = '#00ff00', style = 'italic' },
-   RegName          = { fg = '#00ff00', style = 'italic' },
-   RegPrefix        = { fg = '#00ff00', style = 'italic' },
+    WhichKeyFloat    = { bg = '#bafeba'},
 
-   FloatBorder      = { bg = 'None', fg = 'red' },
+    WildMenu         = { bg = 'none', fg = '#00ff00', style = 'italic' },
 
-   HopNextKey       = { bg = 'none', fg = '#ff4722', style = 'italic' },
-   HopNextKey1      = { bg = 'none', fg = '#00ff00', style = 'italic' },
-   HopNextKey2      = { bg = 'none', fg = '#ff4722', style = 'italic' },
+    RegTitle         = { fg = '#00ff00', style = 'italic' },
+    RegName          = { fg = '#00ff00', style = 'italic' },
+    RegPrefix        = { fg = '#00ff00', style = 'italic' },
 
-   ColorColumn      = { bg = '#e9e9e9', fg = 'NONE', style = 'bold' },
+    FloatBorder      = { bg = 'None', fg = 'red' },
 
-   rainbowcol1      = { fg = '#ff2a2f' },
-   rainbowcol2      = { fg = '#073de6' },
-   rainbowcol3      = { fg = '#12d22e' },
-   rainbowcol4      = { fg = '#ff9900' },
-   rainbowcol5      = { fg = '#ae81ff' },
-   rainbowcol6      = { fg = '#d70084' },
-   rainbowcol7      = { fg = '#00adad' },
+    HopNextKey       = { bg = 'none', fg = '#ff4722', style = 'italic' },
+    HopNextKey1      = { bg = 'none', fg = '#00ff00', style = 'italic' },
+    HopNextKey2      = { bg = 'none', fg = '#ff4722', style = 'italic' },
 
-   SpellBad         = { style = 'undercurl', sp = '#5aaa60' },
-   SpellRare01      = { bg = '#552525', style = 'undercurl', sp = '#5aaa60' },
-   SpellRare02      = { bg = '#402526', style = 'undercurl', sp = '#3a6a30' },
-   SpellRare03      = { bg = '#292426', style = 'undercurl', sp = '#2a5a30' },
+    ColorColumn      = { bg = '#e9e9e9', fg = 'NONE', style = 'bold' },
 
-   Search           = { fg = 'NONE', bg = '#653e1a', style = 'bold',  },
+    rainbowcol1      = { fg = '#ff2a2f' },
+    rainbowcol2      = { fg = '#073de6' },
+    rainbowcol3      = { fg = '#12d22e' },
+    rainbowcol4      = { fg = '#ff9900' },
+    rainbowcol5      = { fg = '#ae81ff' },
+    rainbowcol6      = { fg = '#d70084' },
+    rainbowcol7      = { fg = '#00adad' },
 
-   Whitespace       = { fg = '#353522' },
 
-   DeniteCursorLine = { bg = '#483232' },
-   DenitePathHead   = { fg = '#3233ff' },
-   DenitePathTail   = { fg = '#ff7032' },
+    TabLineSel       = { fg = '#000000', bg = '#ffffff' },
+    TabLine          = { fg = '#abb2bf', bg = '#3e4452' },
+    TabLineFill      = { bg = '#3e4452' },
 
-   PmenuSbar        = { style = 'bold', fg = 'red', bg = 'green' },
+    SpellBad         = { style = 'undercurl', sp = '#5aaa60' },
+    SpellRare01      = { bg = '#552525', style = 'undercurl', sp = '#5aaa60' },
+    SpellRare02      = { bg = '#402526', style = 'undercurl', sp = '#3a6a30' },
+    SpellRare03      = { bg = '#292426', style = 'undercurl', sp = '#2a5a30' },
 
-   rnowebChunk      = { fg = '#a85656' },
+    Search           = { fg = 'NONE', bg = '#f0f08a', style = 'bold',  },
 
-   FloatermBorder   = { bg = 'NONE', fg = '#9696ff', style = 'bold' },
-   Floaterm         = { bg = 'black', fg = '#00ff00' },
-   MYQFNormal       = { fg = '#d8d8d2', bg = '#121212' },
-   MYQFLineNr       = { fg = '#f0f08a', bg = '#674842' },
-   LineNr           = { fg = '#90908a', bg = '#3c3d37', style = 'italic' },
+    Whitespace       = { fg = '#353522' },
 
-   LspDiagnosticsSignError       = { fg = '#ff7780', bg = '#504945', style = 'bold' },
-   LspDiagnosticsSignWarning     = { fg = '#ff7780', bg = '#504945', style = 'bold' },
-   LspDiagnosticsSignInformation = { fg = '#ff7780', bg = '#504945', style = 'bold' },
-   LspDiagnosticsSignHint        = { fg = '#ff7780', bg = '#504945', style = 'bold' },
+    DeniteCursorLine = { bg = '#483232' },
+    DenitePathHead   = { fg = '#3233ff' },
+    DenitePathTail   = { fg = '#ff7032' },
 
-   SignColumn                    = { fg = 'NONE', bg = '#504945' },
-   GitSignsAdd                   = { fg = '#00ff00', bg = '#504945', style = 'bold' },
-   GitSignsDelete                = { fg = '#ee1022', bg = '#504945', style = 'bold' },
-   GitSignsChange                = { fg = '#ffff00', bg = '#504945', style = 'bold' },
-   GitSignsCurrentLineBlame      = { fg = '#504945', style = 'italic' },
+    PmenuSbar        = { style = 'bold', fg = 'red', bg = 'green' },
 
-   GitSignsAddLn                 = { fg = 'NONE', bg = '#225522' },
-   GitSignsChangeLn              = { fg = 'NONE', bg = '#556622' },
-   GitSignsDeleteLn              = { fg = 'NONE', bg = '#502020' }
+    rnowebChunk      = { fg = '#a85656' },
+
+    FloatermBorder   = { bg = 'NONE', fg = '#9696ff', style = 'bold' },
+    Floaterm         = { bg = 'black', fg = '#00ff00' },
+    MYQFNormal       = { fg = '#d8d8d2', bg = '#121212' },
+    MYQFLineNr       = { fg = '#f0f08a', bg = '#674842' },
+    LineNr           = { fg = '#90908a', bg = '#3c3d37', style = 'italic' },
+
+    LspDiagnosticsSignError       = { fg = '#ff7780', bg = '#504945', style = 'bold' },
+    LspDiagnosticsSignWarning     = { fg = '#ff7780', bg = '#504945', style = 'bold' },
+    LspDiagnosticsSignInformation = { fg = '#ff7780', bg = '#504945', style = 'bold' },
+    LspDiagnosticsSignHint        = { fg = '#ff7780', bg = '#504945', style = 'bold' },
+
+    SignColumn                    = { fg = 'NONE', bg = '#504945' },
+    GitSignsAdd                   = { fg = '#00ff00', bg = '#504945', style = 'bold' },
+    GitSignsDelete                = { fg = '#ee1022', bg = '#504945', style = 'bold' },
+    GitSignsChange                = { fg = '#ffff00', bg = '#504945', style = 'bold' },
+    GitSignsCurrentLineBlame      = { fg = '#504945', style = 'italic' },
+
+    GitSignsAddLn                 = { fg = 'NONE', bg = '#225522' },
+    GitSignsChangeLn              = { fg = 'NONE', bg = '#556622' },
+    GitSignsDeleteLn              = { fg = 'NONE', bg = '#502020' }
 }
 
 local function highlight(group, color)
-   local style = color.style and 'gui=' .. color.style or 'gui=NONE'
-   local fg    = color.fg and 'guifg=' .. color.fg or 'guifg=NONE'
-   local bg    = color.bg and 'guibg=' .. color.bg or 'guibg=NONE'
-   local sp    = color.sp and 'guisp=' .. color.sp or ''
+    local style = color.style and 'gui=' .. color.style or 'gui=NONE'
+    local fg    = color.fg and 'guifg=' .. color.fg or 'guifg=NONE'
+    local bg    = color.bg and 'guibg=' .. color.bg or 'guibg=NONE'
+    local sp    = color.sp and 'guisp=' .. color.sp or ''
 
-   vim.api.nvim_command(
-      [[highlight ]] 
-      .. group .. ' ' .. style .. ' '
-      .. fg .. ' ' .. bg .. ' ' .. sp
-   )
+    vim.api.nvim_command(
+        [[highlight ]] 
+        .. group .. ' ' .. style .. ' '
+        .. fg .. ' ' .. bg .. ' ' .. sp
+    )
 end
 
 vim.cmd('au FileType denite-filter setlocal winhighlight=Normal:Folded')
 vim.cmd('au FileType denite,denite-filter setlocal winhighlight=CursorLine:DeniteCursorLine')
 
 t.hook = function()
-   for group,colors in pairs(syntax) do
-     highlight(group,colors)
-   end
+    for group,colors in pairs(syntax) do
+        highlight(group,colors)
+    end
 end
 
 return t
