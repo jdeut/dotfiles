64f7c9ce3ed82ce469fd7e3429cbb6615defbbce
Johannes Deutsch <johannes.deutsch@uni-due.de>
2021-09-28 08:12:19 +0200

    continue restructuring

---
+++ neovim/.config/nvim/ftplugin/lua/lua.lua
@@ -0,0 +1,8 @@
+vim.opt_local.tabstop        = 4
+
+vim.opt_local.foldlevel      = 2
+vim.opt_local.foldmethod     = 'expr'
+vim.opt_local.foldexpr       = 'nvim_treesitter#foldexpr()'
+vim.opt_local.foldenable     = true
+
+vim.opt.foldlevelstart = 2
