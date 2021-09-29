17faa230c08a4c0ebccb34e14faff7563c881053
Johannes Deutsch <johannes.deutsch@uni-due.de>
2021-09-29 23:07:17 +0200

    restructure whichkey mappings

---
+++ neovim/.config/nvim/lua/pluginconfig/whichkey/register/main.lua
@@ -0,0 +1,38 @@
+wk = require("which-key")
+
+require'which-key.plugins.registers'.registers =  '+"-:.%/#=_adfjkn0123789'
+
+wk.setup {
+    plugins = {
+        marks = true,
+        registers = true,
+    },
+    window = {
+        margin = { 0, 10, 4, 10 },
+        padding = { 0, 0, 0, 0 },
+        border = 'single'
+    },
+    layout = {
+        height = { min = 4, max = 22 }, -- 
+        width = { min = 5, max = 40 }, -- 
+        spacing = 5, -- spacing between columns
+        align = "center",
+    },
+    hidden = {
+        "<silent>", 
+        "execute", "<cmd>", "<Cmd>", 
+        "<CR>", "call", "lua", "^:", "^ "
+    },
+    triggers_blacklist = {
+        i = { [[']] }
+    },
+    show_help = true
+}
+
+require'pluginconfig.whichkey.register.init'
+require'pluginconfig.whichkey.register.wintab'
+require'pluginconfig.whichkey.register.browser'
+require'pluginconfig.whichkey.register.display'
+require'pluginconfig.whichkey.register.denite'
+require'pluginconfig.whichkey.register.hop'
+require'pluginconfig.whichkey.register.floaterm'
