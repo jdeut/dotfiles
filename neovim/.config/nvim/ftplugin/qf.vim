33cb8b0a38125357ddacf51d11855cca5ed67e1e
Johannes Deutsch <johannes.deutsch@uni-due.de>
2021-10-14 15:11:17 +0200

    quickfix specific conf from vim to lus

--- neovim/.config/nvim/ftplugin/qf.vim
+++
@@ -1,5 +0,0 @@
-resize 6
-setlocal nowrap
-setlocal nonumber
-setlocal colorcolumn=0
-setlocal winhighlight=Normal:MYQFNormal
---
+++ neovim/.config/nvim/ftplugin/qf/qf.lua
@@ -0,0 +1,8 @@
+-- set current window height to
+vim.cmd([[resize 8]])
+
+vim.wo.wrap         = true
+vim.wo.number       = false
+vim.wo.colorcolumn  = ''
+vim.wo.showbreak    = '>>>> '
+vim.wo.winhighlight = [[NonText:MyQfNonText]]
