36be57547f2d8ceb16f49fde774c8119400970cf
Johannes Deutsch <johannes.deutsch@uni-due.de>
2021-10-20 17:21:13 +0200

    formatting

--- neovim/.config/nvim/lua/pluginconfig/whichkey/init.lua
+++ neovim/.config/nvim/lua/pluginconfig/whichkey/init.lua
@@ -3,32 +3,32 @@ wk = require("which-key")
 require'which-key.plugins.registers'.registers =  '+"-:.%/#=_adfjkn0123789'
 
 wk.setup {
-    plugins = {
-        marks = false,
-        registers = true,
-    },
-    window = {
-        margin = { 0, 10, 4, 10 },
-        padding = { 0, 0, 0, 0 },
-        border = 'single'
-    },
-    layout = {
-        height = { min = 4, max = 22 }, -- 
-        width = { min = 5, max = 40 }, -- 
-        spacing = 5, -- spacing between columns
-        align = "center",
-    },
-    hidden = {
-        "execute", "<cmd>", "<Cmd>", 
-        "<CR>", "call", "lua", "^:", "^ "
-    },
-    triggers = {
-        '<localleader>', '<leader>', 'z', 'g', ']', '[', [[']], [[`]]
-    },
-    triggers_blacklist = {},
-    ignore_missing = true,
-    motions = { count = false },
-    show_help = true
+   plugins = {
+      marks = false,
+      registers = true,
+   },
+   window = {
+      margin = { 0, 10, 4, 10 },
+      padding = { 0, 0, 0, 0 },
+      border = 'single'
+   },
+   layout = {
+      height = { min = 4, max = 22 }, -- 
+      width = { min = 5, max = 40 }, -- 
+      spacing = 5, -- spacing between columns
+      align = "center",
+   },
+   hidden = {
+      "execute", "<cmd>", "<Cmd>", 
+      "<CR>", "call", "lua", "^:", "^ "
+   },
+   triggers = {
+      '<localleader>', '<leader>', 'z', 'g', ']', '[', [[']], [[`]]
+   },
+   triggers_blacklist = {},
+   ignore_missing = true,
+   motions = { count = false },
+   show_help = true
 }
 
 require'pluginconfig.whichkey.register.main'
