873ba141aeb27242d50d5ca78a63b1120475bfd0
Johannes Deutsch <johannes.deutsch@uni-due.de>
2021-12-19 03:09:47 +0100

    update fzf-lua settings

--- neovim/.config/nvim/lua/pluginconfig/fzf-lua.lua
+++ neovim/.config/nvim/lua/pluginconfig/fzf-lua.lua
@@ -1,93 +1,64 @@
-t = {}
+local t = {}
 
 local actions = require'fzf-lua.actions'
-local path    = require'fzf-lua.path'
+local helper  = require'myhelper'
 
 t.custom = {}
 
 t.custom.git_files = function(opts)
 
-   local merged_opts
-
    local default_opts = {
-      prompt             = 'GitFiles❯ ',
-      git_icons          = true,           -- show git icons?
-      file_icons         = true,           -- show file icons?
-      color_icons        = true, -- colorize file|git icon,
-      winopts = {
-         preview = {
-            hidden = 'nohidden',
-            layout = 'vertical'
-         }
-      }
+      prompt      = 'GitFiles❯ '
    }
 
-   if not type(opts) == 'table' then
-      merged_opts = default_opts
-   else
-      merged_opts = vim.tbl_deep_extend('force', default_opts, opts or {})
-   end
-
-   if not merged_opts.cwd then
-      merged_opts.cwd = path.git_root(vim.fn.expand('%:h:.'))
-
-      if not merged_opts.cwd then
-      return
-      end
-   end
+   local merged_opts = type(opts) == 'table' and
+      vim.tbl_deep_extend('force', default_opts, opts or {}) or
+      default_opts
 
    local exclude = not vim.fn.expand('%:.') == ''
       and '--exclude "' .. vim.fn.expand('%:.') .. '" '
       or ''
 
+   merged_opts.cwd = merged_opts.cwd and merged_opts.cwd or
+      helper.git_get_root_or_cwd()
+
    merged_opts.cmd = table.concat({
       'fdfind -t f -H ',
       exclude,
-      '--base-directory "', merged_opts.cwd or vim.env.PWD , '" ',
+      '--base-directory "',
+      merged_opts.cwd or vim.env.PWD , '" ',
       '--exec stat --printf="%Y\t%n\n"',
       ' | sort -r -k 1 | cut -f 2'
    })
 
-   --
-   --
    require'fzf-lua'.files(merged_opts)
 end
 
-local floatArgs = function()
-   local columns = vim.o.columns
-   local lines = vim.o.lines
+local floatMultiColArgs = function()
 
-   local crow, ccol = require'Fn.helper'.get_screen_cursor_position()
+   local w = { max = 125, col1 = 40 }
 
-   local width = columns < 110
-      and math.floor(columns - (columns * 1 / 10))
-      or 110
-
-   local height = lines < 25
-      and lines - 10
-      or  25 - 10
-
-   local col =  ( math.abs(ccol - .5 * columns) > 25 and ccol or .5 * columns ) - .5 * width
-   local row =  crow  - .5 * height
-
-   print(row)
+   w.float = vim.opt.columns:get() * 0.8 > w.max and w.max
+         or vim.opt.columns:get() * 0.8
+   w.floatr = math.floor(w.float / vim.opt.columns:get() * 100 ) / 100
+   w.col2 = math.floor( (w.float - w.col1) / w.float * 100)
 
    return {
-      relative = 'editor',
-      row      = row,
-      col      = math.floor(col),
-      width    = width,
-      height   = height,
-      style    = 'minimal'
+      layout     = ( w.float > 100 and 'horizontal' or 'vertical' ),
+      horizontal = string.format('right:%d%%', w.col2 ),
+      width      = w.floatr,
+      vertical   = 'down:40%'
    }
 end
 
 require'fzf-lua'.setup({
-   -- fzf_args       = '--color=dark,preview-fg:#00ff00',
-   winopts_raw    = floatArgs,
+   global_resume  = true,
    git_icons      = true,           -- show git icons?
    file_icons     = true,           -- show file icons?
    color_icons    = true,           -- colorize file|git icons
+   actions = {
+      ['ctrl-q'] = function(s) helper.gxmessage(s) end
+   },
    keymap = {
       fzf = {               -- fzf '--bind=' options
          ['f2']         = 'toggle-preview',
@@ -95,25 +66,53 @@ require'fzf-lua'.setup({
          ['ctrl-d']     = 'half-page-down',
          ['ctrl-u']     = 'half-page-up',
          ['ctrl-a']     = 'toggle-all',
-         ['ctrl-space'] = 'toggle',
+         ['ctrl-r']     = 'toggle+down',
          ['ctrl-j']     = 'down',
          ['ctrl-k']     = 'up',
-         ['ctrl-l']     = 'clear-query'
+         ['ctrl-h']     = 'clear-query'
       }
    },
+   fzf_opts = {
+      -- options are sent as `<left>=<right>`
+      -- set to `false` to remove a flag
+      -- set to '' for a non-value flag
+      -- for raw args use `fzf_args` instead
+      ['--ansi']        = '',
+      ['--prompt']      = '> ',
+      ['--info']        = 'inline',
+      ['--height']      = '100%',
+      ['--layout']      = 'reverse'
+   },
+   fzf_colors = {
+      ["bg"]      = { "bg", "MyFzfBg" },
+      ["fg"]      = { "fg", "MyFzfFg" },
+      ["hl"]      = { "fg", "MyFzfInfo"},
+      ["bg+"]     = { "bg", "MyFzfBgPlus" },
+      ["fg+"]     = { "fg", "MyFzfFgPlus" },
+      ["hl+"]     = { "fg", "MyFzfHighPlus" },
+      ["info"]    = { "fg", "MyFzfInfo" },
+      ["prompt"]  = { "fg", "MyFzfPrompt" },
+      ["pointer"] = { "fg", "MyFzfPointer" },
+      ["marker"]  = { "fg", "MyFzfMarker" },
+      ["spinner"] = { "fg", "MyFzfSpinner" },
+      ["header"]  = { "fg", "MyFzfSpinner" },
+      ["gutter"]  = { "bg", "MyFzfGutter" },
+   },
    winopts = {
-      border = false,
+      border = 'none',
       hl = {
-         normal = 'FzfLuaDefault',
-         border = 'Folded'
+         normal = 'MyFzfLuaDefault',
+         border = 'MyFzfLuaBorder'
       },
       preview = {
-         default        = 'cat',
-         border         = 'border',
+         default        = 'bat',
+         border         = 'noborder',
          hidden         = 'nohidden',
-         preview_layout = 'horizontal',
+         layout         = 'horizontal',
          vertical       = 'down:20%',
-         scrollbar      = 'border'
+         horizontal     = 'right:40%',
+         scrollbar      = 'border',
+         title          = false
       },
       on_create = function()
             mappings = {
@@ -124,8 +123,19 @@ require'fzf-lua'.setup({
                ['<S-PageDown>'] = { function()
                      require'fzf-lua.win'.preview_scroll(1)
                   end, ''
+               },
+               ['`'] = {
+                  name = '',
+                  a = { function()
+                        helper.gxmessage('sss')
+                     end, 'ss'
+                  }
                }
             }
+
+            vim.opt_local.buflisted = false
+            vim.opt_local.bufhidden = 'wipe'
+            vim.opt_local.signcolumn = 'no'
 
             require('which-key').register(mappings, {
                buffer = 0,
@@ -141,30 +151,44 @@ require'fzf-lua'.setup({
       },
       bat = {
          cmd             = "bat",
-         args            = "--style=numbers,changes --color always",
-         theme           = 'zenburn' -- bat --list-themes
+         args            = "--style numbers --color always",
+         theme           = 'zenburn', -- bat --list-themes,
+         config          = nil,            -- nil uses $BAT_CONFIG_PATH
+      },
+      builtin = {
+         syntax          = true,         -- preview syntax highlight?
+         syntax_limit_l  = 0,            -- syntax limit (lines), 0=nolimit
+         syntax_limit_b  = 1024*1024    -- syntax limit (bytes), 0=nolimit
+      },
+      man = {
+         cmd = "man -c %s | col -bx"
       }
+   },
+   files = {
+      multiprocess = true
    },
    git = {
       files = {
          prompt = 'GitFiles❯ ',
-         winopts = {
-            preview = {
-               default = 'bat',
-               layout = 'horizontal',
-               title = false,
-               hidden = 'nohidden',
-               border = 'noborder'
-            }
-         }
+         multiprocess = true
+      }
+   },
+   helptags = {
+      -- winopts_raw = floatMultiColArgs,
+      fzf_opts = {
+         ['--tiebreak'] = 'index'
+      }
+   },
+   manpages = {
+      previewer = {
+         _ctor = require'fzf-lua.previewer'.fzf.man_pages
       }
    },
    buffers = {
-      default = 'bat',
       winopts = {
          preview = {
             preview_layout = 'horizontal',
-            preview_horizontal = 'down:18%',
+            preview_horizontal = 'down:28%',
             ignore_current_buffer = true,
             no_term_buffers = false
          }
@@ -173,25 +197,17 @@ require'fzf-lua'.setup({
    grep = {
       prompt       = 'Rg❯ ',
       input_prompt = 'Grep For❯ ',
-      rg_opts      = "--hidden --column --line-number --no-heading " ..
-                     "--color=always --smart-case -g '!{.git,node_modules}/*'",
+      rg_opts      = "-g '!{.git,node_modules}/*'",
       git_icons    = true,           -- show git icons?
       file_icons   = true,           -- show file icons?
       color_icons  = true,           -- colorize file|git icons
+      multiprocess = true,
       no_esc       = 2,
-      -- preview_opts = 'hidden',
       actions      = {
          ['ctrl-q']  = actions.file_sel_to_qf,
       },
       winopts = {
-         preview = {
-            default = 'bat',
-            title = false,
-            hidden = 'nohidden',
-            border = 'border',
-            layout = 'vertical',
-            vertical = 'down:25%'
-         }
+         split = 'belowright new'
       }
    },
    lsp = {
@@ -203,7 +219,9 @@ require'fzf-lua'.setup({
       git_icons         = false,
       lsp_icons         = true,
       severity          = "hint",
-      fzf_opts = { ['--keep-right'] = '' },
+      fzf_opts = {
+         ['--keep-right'] = ''
+      },
       icons = {
          ["Error"]       = { icon = "", color = "red" },       -- error
          ["Warning"]     = { icon = "", color = "yellow" },    -- warning
@@ -212,10 +230,6 @@ require'fzf-lua'.setup({
       },
       winopts = {
          preview = {
-            default = 'bat',
-            title = false,
-            hidden = 'nohidden',
-            border = 'border',
             layout = 'vertical',
             vertical = 'down:25%'
          }
@@ -223,13 +237,8 @@ require'fzf-lua'.setup({
    },
    oldfiles = {
       cwd = vim.env.HOME,
-      winopts = {
-         preview = {
-            default = 'bat',
-            title = false,
-            hidden = 'hidden',
-            border = 'noborder'
-         }
+      fzf_opts = {
+         ['--tiebreak'] = 'index',
       }
    }
 })
