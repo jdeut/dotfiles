af4a7d10f144bd1b257241f761e053865b341c05
Johannes Deutsch <johannes.deutsch@uni-due.de>
2021-12-20 03:47:46 +0100

    fix video to gif convert script

--- local-bin-scripts/.local/bin/webp-to-gif.sh
+++ local-bin-scripts/.local/bin/webp-to-gif.sh
@@ -1,27 +1,49 @@
 #!/bin/zsh
 
+set -x
+
 if [ $# -eq 0 ]
 then
     echo "Please pass a filename to the program"
     exit 1
 fi
 
-TFILE=$(mktemp --suffix=".mp4")
-TNEW=$(mktemp --suffix=".mp4")
-TGIFFILE=$(mktemp --suffix=".gif")
-PALETTE=$(mktemp --suffix=".png")
+ffmpeg_gif () {
+    # ext="${1##*.}"
+    # TFILE="${1%.*}_.$ext"
+    OUT="$2"
+    rate="$3"
+    filters="setpts=${rate}*PTS"
+    TNEW=$(mktemp --suffix=".mp4")
+    PALETTE=$(mktemp --suffix=".png")
+    TGIFFILE="${1%.*}_.gif"
 
-mpv --stream-record="$TFILE" "$@"
+    ffmpeg -y -i "$1" -vf "palettegen=stats_mode=diff" "$PALETTE"
+    ffmpeg -y -i "$1" -i "$PALETTE" -filter_complex "[0:v]${filters}[filtered],[filtered][1:v] paletteuse" "$TGIFFILE"
+
+    gifsicle -i --color-method=median-cut --colors 226 --lossy=35 -O3 "$TGIFFILE" > "$OUT"
+
+    rm "$TGIFFILE"
+}
+
+
+# mpv --stream-record="$TFILE" "$@"
 
 RATE=$(gxmessage -entrytext 1 Speed)
 
+ffmpeg_gif "$@" "${@%.*}_oneway.gif" "$RATE"
 
-GENNEW="ffmpeg -y -i \"$TFILE\" -filter:v \"setpts=$RATE*PTS,fps=fps=17\" -movflags faststart \"$TNEW\""
-GENPALETTE="ffmpeg -y -i \"$TNEW\" -vf \"palettegen=stats_mode=diff\" \"$PALETTE\""
-GENGIF="ffmpeg -y -i \"$TNEW\" -i \"$PALETTE\" -filter_complex \"[0:v][1:v] paletteuse\" \"$TGIFFILE\""
+################
 
-urxvt -e bash -c "$GENNEW ; $GENPALETTE ; $GENGIF ; gifsicle -O3 $TGIFFILE -o $TGIFFILE"
+bounce="${@%.*}_new.mp4"
 
-nohup eog $TGIFFILE
+ffmpeg -i "$@" -filter_complex "[0:v]reverse,fifo[r];[0:v][r] concat=n=2:v=1 [v]" -map "[v]" "$bounce"
+
+ffmpeg_gif "$bounce" "${@%.*}_bounce.gif" "$RATE"
+
+rm "$bounce"
+
+
+nohup eog "$TGIFFILE_BOUNCE"
 
 #RET=$(wl-copy < "$TGIFFILE")
