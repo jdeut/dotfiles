6f93b1bc3c4ca9dcf7f8a2754f627ad9ca0782f8
Johannes Deutsch <johannes.deutsch@uni-due.de>
2022-01-05 11:41:46 +0100

    update scripts

--- local-bin-scripts/.local/bin/open_raise_nvim.sh
+++ local-bin-scripts/.local/bin/open_raise_nvim.sh
@@ -2,27 +2,14 @@
 
 # gxmessage $PATH
 #
-if [ $# -eq 0 ]
-then
+if [ $# -eq 0 ] ; then
     gxmessage "Please pass a filename to the program"
     exit 1
-fi
-
-if [ ! -f "$@" ]
-then
+elif [ ! -f "$@" ] ; then
     gxmessage "$@ does not exist"
     exit 1
 fi
 
-# pgrep nvim-qt > /dev/null
-
-# if [ $? -eq 1 ]; then
-#     #gxmessage nnono
-#     gtk-launch nvim-qt
-# fi
-
-$HOME/.local/bin/nvr "$@"
+$HOME/.local/bin/nvr --remote-expr "luaeval('require[[myhelper]].open_from_ext([[$@]])')"
 
 /home/johannes/.local/bin/run-raise-bridge.sh nvim
-
-# tmux select-window -t Primary:1
--- local-bin-scripts/.local/bin/run-raise-bridge.sh
+++ local-bin-scripts/.local/bin/run-raise-bridge.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-focusedapp=$(get_focused_app.sh)
+# focusedapp=$(get_focused_app.sh)
 
-echo "#######################################"
-echo "# $1  -  $focusedapp "
-echo "#######################################"
+echo "# >> $1  -  $focusedapp "
 
 tmux list-windows -F "#I-#{window_active}" -t Primary | grep 1-1 &> /dev/null
 
@@ -12,54 +10,19 @@ is_nvim_active=$?
 
 case $1 in
     r_x11)
-        
         ydotool key "super+ctrl+alt+r"
     ;;
     sakura_primary)
-#        if [[ "$focusedapp" -eq "SakuraPrimary" ]] && [[ "$is_nvim_active" -eq "0" ]]
-#        then
-#            ydotool key "ctrl+b"
-#            ydotool key "l"
-#        else
-#            tmux select-window -t Primary:1
-#            ydotool key "super+ctrl+alt+1"
-#        fi
-
-        if [ "$focusedapp" = "SakuraPrimary" ] 
-        then
-            #ydotool key --up 1
-            #
-            #ydotool key --up 1
-            #ydotool key --up super
-            #ydotool key --up super
-            #ydotool key --up super
-            ydotool sleep 150 , key ctrl+alt+b
-        else
-            echo "## <---- "
-            #ydotool key "x"
-            #ydotool key --up "super+1"
-            ydotool sleep 150 , key ctrl+alt+a
-        fi
+        ydotool sleep 100 , key "super+ctrl+alt+0" &> /dev/null
     ;;
-
     sakura_secondary)
-        if [ "$focusedapp" = "SakuraSecondary" ] 
-        then
-            ydotool sleep 150 , key ctrl+alt+a
-        else
-            echo "## <---- "
-            ydotool sleep 150 , key ctrl+alt+b
-        fi
+        ydotool sleep 100 , key "super+ctrl+alt+a" &> /dev/null
     ;;
-
     nvim)
-        ydotool sleep 80 , key "ctrl+alt+0"
+        ydotool sleep 100 , key "super+ctrl+alt+0" &> /dev/null
         tmux select-window -t Primary:1
     ;;
-
     okular)
-        ydotool sleep 80 , key "ctrl+alt+9"
+        ydotool sleep 100 , key "super+ctrl+alt+9" &> /dev/null
     ;;
 esac
-
-
