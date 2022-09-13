6ea255624da812325d84db537a6c54bad7c9b079
Johannes Deutsch <johannes.deutsch@uni-due.de>
2022-09-13 08:52:28 +0200

    latest

---
+++ luarocks/.config/luarocks/config-5.1.lua
@@ -0,0 +1,11 @@
+local_by_default = true
+lua_interpreter = 'lua5.1'
+rocks_trees = {
+   {
+      root = '/home/johannes/.luarocks'
+   },
+   {
+      root = '/usr',
+      lib_dir = '/usr/lib/x86_64-linux-gnu/lua/' .. lua_version
+   },
+}
