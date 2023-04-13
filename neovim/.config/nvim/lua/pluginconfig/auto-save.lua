b2b506d4d902651b62219a8fe282f50d52f418fe
Johannes Deutsch <johannes.deutsch@uni-due.de>
2023-04-13 13:37:57 +0200

    adjust auto-save config

--- neovim/.config/nvim/lua/pluginconfig/auto-save.lua
+++ neovim/.config/nvim/lua/pluginconfig/auto-save.lua
@@ -1,24 +1,21 @@
 local autosave = require('auto-save')
 
-vim.g.auto_save_silent = 1
-
-autosave.hook_after_saving = function()
-   autosave.setup({
-      execution_message = 'AutoSave: ' .. vim.fn.strftime('%H:%M:%S')
-   })
-end
-
 autosave.setup( {
    enabled                     = true,
    debounce_delay              = 50,
-   on_off_commands             = true,
-   execution_message           = 'AutoSave: ' .. vim.fn.strftime('%H:%M:%S'),
+   -- on_off_commands             = true,
+   execution_message           = {
+      message = function()
+         return 'AutoSave: ' .. vim.fn.strftime('%H:%M:%S')
+      end,
+      cleaning_interval = 2000
+   },
    write_all_buffers           = false,
    clean_command_line_interval = 3000,
-   events = {
-      'InsertLeave',
-      'WinLeave',
-      'TextChanged'
+   trigger_events = {
+      immediate_save = { 'InsertLeave', 'WinLeave'},
+      defer_save = { 'TextChanged' },
+      cancel_defered_save = { "InsertEnter" }
    },
    conditions = {
       exists = true,
