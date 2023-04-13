62ad87e3041e2d6638831f9d6ed3309031906f80
Johannes Deutsch <johannes.deutsch@uni-due.de>
2023-04-13 12:09:25 +0200

    run `IndentBlanklineDisable` only if cmd exists

--- neovim/.config/nvim/ftplugin/packer/packer.lua
+++ neovim/.config/nvim/ftplugin/packer/packer.lua
@@ -1,6 +1,8 @@
 vim.wo.winhighlight = 'Normal:WhichKeyGroup'
 
-vim.cmd([[IndentBlanklineDisable]])
+if vim.fn.exists(':IndentBlanklineDisable') > 0 then
+   vim.cmd([[IndentBlanklineDisable]])
+end
 
 vim.defer_fn( function()
       vim.wo.wrap = true
