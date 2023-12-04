73f04207fc61f8fc02278506a3cc67883bfdfa5a
Johannes Deutsch <johannes.deutsch@uni-due.de>
2023-12-04 22:51:06 +0100

    register mappings on LspAttach

--- neovim/.config/nvim/lua/pluginconfig/which-key/register/autocmd/lspattach.lua
+++ neovim/.config/nvim/lua/pluginconfig/which-key/register/autocmd/lspattach.lua
@@ -1,5 +1,5 @@
 local set_keymap = function(bufnr, mode, prefix, map)
-   require('which-key').register( map,
+   require('which-key').register(map,
       { mode = mode, buffer = bufnr, prefix = prefix }
    )
 end
@@ -16,9 +16,41 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
       local caps = client.server_capabilities
 
+      -- require 'myhelper'.gxmessage(caps)
+
+      set_keymap(bufnr, 'n', '<localleader>', {
+         ['wl'] = {
+            function()
+               vim.print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+            end,
+            'Lsp List Worspace Folders'
+         }
+      })
+
+      if client.supports_method("textDocument/formatting") then
+         set_keymap(bufnr, 'n', '<localleader>', {
+            ['f'] = {
+               function()
+                  vim.lsp.buf.format { async = false }
+               end,
+               'Lsp Format Code'
+            }
+         })
+      end
+
       if caps.hoverProvider then
          set_keymap(bufnr, 'n', 'g', {
             h = { vim.lsp.buf.hover, 'Lsp Hover' }
+         })
+      end
+      if caps.signatureHelpProvider then
+         set_keymap(bufnr, 'n', 'g', {
+            ['s'] = { vim.lsp.buf.signature_help, 'Lsp Signature Help' }
+         })
+      end
+      if caps.renameProvider then
+         set_keymap(bufnr, 'n', '<leader>', {
+            ['er'] = { vim.lsp.buf.rename, 'Lsp Rename' }
          })
       end
       if caps.documentSymbolProvider then
--- neovim/.config/nvim/lua/pluginconfig/which-key/register/autocmd/lspattach_texlab.lua
+++ neovim/.config/nvim/lua/pluginconfig/which-key/register/autocmd/lspattach_texlab.lua
@@ -1,5 +1,5 @@
 local set_keymap = function(bufnr, mode, prefix, map)
-   require('which-key').register( map,
+   require('which-key').register(map,
       { mode = mode, buffer = bufnr, prefix = prefix }
    )
 end
@@ -17,15 +17,15 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
       set_keymap(bufnr, 'n', '<localleader>', {
          b = {
-            function ()
+            function()
                vim.cmd([[TexlabBuild]])
             end, 'TexlabBuild' }
       })
       set_keymap(bufnr, 'n', '<localleader>', {
          f = {
-            function ()
+            function()
                vim.cmd([[TexlabForward]])
-            end, 'TexlabBuild' }
+            end, 'TexlabForward' }
       })
    end,
 })
