97b2279b7a571fa89e52580f52803b3f3a67685b
Johannes Deutsch <johannes.deutsch@uni-due.de>
2023-12-05 09:12:30 +0100

    remove unused stuff

--- neovim/.config/nvim/lua/style/sign.lua
+++
@@ -1,8 +0,0 @@
-local t = {
-   DiagnosticSignError = { text = "ᵉ", texthl = "DiagnosticSignError" },
-   DiagnosticSignWarn  = { text = "ʷ", texthl = "DiagnosticSignWarn" },
-   DiagnosticSignInfo  = { text = "ⁱ", texthl = "DiagnosticSignInfo" },
-   DiagnosticSignHint  = { text = "ʰ", texthl = "DiagnosticSignHint" }
-}
-
-return t
